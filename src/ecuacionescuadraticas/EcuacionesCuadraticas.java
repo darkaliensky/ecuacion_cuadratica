2e6e9ea738242cea47b0f8d656372629ab0ec85b
darkaliensky <darkaliensky@Alonso>
2015-10-04 20:10:23 -0300

    Segunda versión, con validaciones

--- src/ecuacionescuadraticas/EcuacionesCuadraticas.java
+++ src/ecuacionescuadraticas/EcuacionesCuadraticas.java
@@ -22,27 +22,47 @@ public class EcuacionesCuadraticas {
         double C;
         double raiz1;
         double raiz2;
-        double ecuacion;
+        double imaginario1;
         String seguir;
+        
         do{
             System.out.println("Dada la ecuación cuadrática de la forma Ax² + Bx + C");
             System.out.println("Ingrese el coeficiente cuadrático (Ax²) de la ecuación");
-            A = leer.nextDouble();
-            System.out.println("Ingrese el coeficiente lineal (Bx) de la ecuación");
-            B = leer.nextDouble();
-            System.out.println("Ingrese el coeficiente númerico (C) de la ecuación");
-            C = leer.nextDouble();
+            try{
+                A = leer.nextDouble();
+                System.out.println("Ingrese el coeficiente lineal (Bx) de la ecuación");
+                B = leer.nextDouble();
+                System.out.println("Ingrese el coeficiente númerico (C) de la ecuación");
+                C = leer.nextDouble();
             
-            raiz1 = (-B) + (Math.sqrt(B*B - 4*A*C));
-            raiz1 = raiz1/ (2*A);
-      
-            raiz2 = (-B) - (Math.sqrt(B*B - 4*A*C));
-            raiz2 = raiz2/ (2*A);
+                
+                if((B*B - 4*A*C) < 0){
+                    raiz1 = (-B) / (2*A);
+                    imaginario1 = Math.sqrt((B*B - 4*A*C) * (-1));
+                    imaginario1 = imaginario1 / (2*A);
+                    
+                    System.out.println("Las soluciones para esta ecuación cuadratica de la forma " +A+"x² + " +B+"x + " +C);
+                    System.out.println("x1 = " +raiz1+ " + " +imaginario1+ "i x2 = " +raiz1+ " - " +imaginario1+ "i");
+                    
+                }else{
+                    raiz1 = (-B) + (Math.sqrt(B*B - 4*A*C));
+                    raiz1 = raiz1/ (2*A);
+                
+                    raiz2 = (-B) - (Math.sqrt(B*B - 4*A*C));
+                    raiz2 = raiz2/ (2*A);
+                
+                    System.out.println("Las soluciones para esta ecuación cuadratica de la forma " +A+"x² + " +B+"x + " +C);
+                    System.out.println("x1 = " +raiz1+ " x2 = " +raiz2);
+                }
+                
+            }
+            catch (Exception e){
+                System.out.println("Ingrese un númereo válido");
+            }
             
-            System.out.println("Las soluciones para esta ecuación cuadratica de la forma " +A+"x² +" +B+"x +" +C);
-            System.out.println("x1 = " +raiz1+ " x2 = " +raiz2);
             System.out.println("¿Desea continuar con otra operación? Presione S para continuar y cualquier tecla para salir");
             seguir = leer.next();
+        
         }while(seguir.equals("s") || seguir.equals("S"));
     }
     
